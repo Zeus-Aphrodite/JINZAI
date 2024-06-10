745cb80e80f9ea703b161b1ad61a4e643b356f40
Zeus-Aphrodite <zeusa1398@gmail.com>
2024-06-10 15:56:52 +0900

    feat/init

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function technical() {
+  window.location.href = "https://tokuteiginou-ajc.site/";
+}
+function data() {
+  window.location.href = "https://tokuteiginou-ajc.site/download/";
+}
+function request() {
+  window.location.href = "https://tokuteiginou-ajc.site/contact/";
+}
